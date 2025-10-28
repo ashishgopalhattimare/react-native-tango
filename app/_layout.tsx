0f8f312ec95281632c6899073a5b9b1971b8cfbb
Ashish Gopal Hattimare <ashishgopalhattimare@gmail.com>
2025-10-28 23:52:37 +0530

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout() {
         <Stack.Screen
           name="home"
           options={{
-            headerShown: isNative,
+            headerShown: false,
           }}
         />
       </Stack>
